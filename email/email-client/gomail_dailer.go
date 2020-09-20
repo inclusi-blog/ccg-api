16ab8c56b070e1292b5da29b4a10798a10f86043
Hariharan Sivakumar <harishsivakumar97@gmail.com>
2020-09-20 11:28:26 +0530

    [Harish] : Add mocks for email clitn

--- email/email-client/gomail_dailer.go
+++ email/email-client/gomail_dailer.go
@@ -1,5 +1,6 @@
 package email_client
 
+// mockgen -source=email/email-client/gomail_dailer.go -destination=email/email-client/mocks/mocks_gomail_dailer.go -package=mocks
 import "gopkg.in/gomail.v2"
 
 type GomailDialer interface {
